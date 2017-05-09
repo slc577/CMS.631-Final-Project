18ff41f0b7b8539016e0c77b6310460af909d0d2
treeshuh <tricia.shi@gmail.com>
2017-05-09 19:36:18 -0400

    basic functionality

---
+++ javascripts/destinations.js
@@ -0,0 +1,74 @@
+var DESTINATIONS = [
+	{
+		"name": "Shaw's",
+		"id": "shaws",
+		"walkTransport": {
+			"time": 11,
+			"money": 0,
+			"carbon": 0
+		},
+		"bikeTransport": {
+			"time": 5,
+			"money": 6,
+			"carbon": 0
+		},
+		"publicTransport": {
+			"time": 7,
+			"money": 1.70,
+			"carbon": 0.27
+		},
+		"carTransport": {
+			"time": 4,
+			"money": 11.25,
+			"carbon": 0.576
+		}
+	},
+	{
+		"name": "Cambridgeside Galleria",
+		"id": "cambridgeside",
+		"walkTransport": {
+			"time": 28,
+			"money": 0,
+			"carbon": 0
+		},
+		"bikeTransport": {
+			"time": 11,
+			"money": 6,
+			"carbon": 0
+		},
+		"publicTransport": {
+			"time": 20,
+			"money": 0,
+			"carbon": 5.04
+		},
+		"carTransport": {
+			"time": 8,
+			"money": 12.38,
+			"carbon": 1.73
+		}
+	},
+	{
+		"name": "Movie at Fenway",
+		"id": "fenwaymovie",
+		"walkTransport": {
+			"time": 30,
+			"money": 0,
+			"carbon": 0
+		},
+		"bikeTransport": {
+			"time": 10,
+			"money": 6,
+			"carbon": 0
+		},
+		"publicTransport": {
+			"time": 25,
+			"money": 2.25,
+			"carbon": 0.63
+		},
+		"carTransport": {
+			"time": 10,
+			"money": 11.15,
+			"carbon": 1.34
+		}
+	},	
+]
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,124 @@
 $(document).ready(function() {
 
+
 var MyModel = function() {
 	var me = this;
-	me.title = ko.observable("Hello World!");
+	me.STATES = {
+		HOME: 0,
+		CHOOSE_ACTIVITIES: 1,
+		FIRST_QUESTIONS: 2,
+		TRANSITION_SCREEN: 3,
+		SECOND_QUESTIONS: 4,
+		RESULTS: 5
+	};
+	me.NUM_ACTIVITIES_TO_PICK = 2;
+	me.DESTINATIONS = ko.observableArray(DESTINATIONS);
+	me.state = ko.observable(me.STATES.HOME);
+	me.pickedActivities = ko.observableArray([]);
+	me.firstResults = ko.observableArray([]);
+	me.secondResults = ko.observableArray([]);
+	me.questionIndex = ko.observable(0);
+
+	me.toggleActivity = function(activity) {
+		for (var i = 0; i < me.pickedActivities().length; i++) {
+			if (me.pickedActivities()[i].id == activity.id) {
+				me.pickedActivities.splice(i, 1); // delete it
+				return;
+			} 
+		}
+		me.pickedActivities.push(activity);
+	}
+
+	me.isPickedActivity = function(activity) {
+		for (var i = 0; i < me.pickedActivities().length; i++) {
+			if (me.pickedActivities()[i].id == activity.id) {
+				return true;
+			} 
+		}
+		return false;
+	}
+
+	me.currentActivity = ko.pureComputed({
+		read: function() {
+			if (me.pickedActivities().length > me.questionIndex()) {
+				return me.pickedActivities()[me.questionIndex()];
+			} else {
+				return null;
+			}
+		}
+	});
+
+	me.selectTransport = function(name, details) {
+		if (me.state() == me.STATES.FIRST_QUESTIONS) {
+			var current = me.firstResults();
+			if (current.length <= me.questionIndex()) {
+				me.firstResults.push({"name": name, "details": details[name]});
+			} else {
+				current[me.questionIndex()] = {"name": name, "details": details[name]};
+				me.firstResults(current);
+			}
+		} else if (me.state() == me.STATES.SECOND_QUESTIONS) {
+			var current = me.secondResults();
+			if (current.length <= me.questionIndex()) {
+				me.secondResults.push({"name": name, "details": details[name]});
+			} else {
+				current[me.questionIndex()] = {"name": name, "details": details[name]};
+				me.secondResults(current);
+			}
+		} else {
+			return; 
+		}
+	}
+
+	me.isPickedTransport = function(name) {
+		if (me.state() == me.STATES.FIRST_QUESTIONS) {
+			var current = me.firstResults();
+		} else if (me.state() == me.STATES.SECOND_QUESTIONS) {
+			var current = me.secondResults();
+		} else {
+			return false;
+		}
+		if (current.length > me.questionIndex()) {
+			return name == current[me.questionIndex()].name;
+		}
+		return false;
+	}
+
+	me.next = function() {
+		if (me.state() == me.STATES.FIRST_QUESTIONS) {
+			me.questionIndex(me.questionIndex()+1);
+			if (me.questionIndex() >= me.NUM_ACTIVITIES_TO_PICK) {
+				me.questionIndex(0);
+				me.state(me.STATES.TRANSITION_SCREEN);
+			}
+		} else if (me.state() == me.STATES.SECOND_QUESTIONS) {
+			me.questionIndex(me.questionIndex()+1);
+			if (me.questionIndex() >= me.NUM_ACTIVITIES_TO_PICK) {
+				me.questionIndex(0);
+				me.state(me.STATES.RESULTS);
+			}
+		}
+	}
+
+	me.totalCarbonFirst = ko.pureComputed({
+		read: function() {
+			var sum = 0;
+			me.firstResults().forEach(function(r) {
+				sum += parseFloat(r.details.carbon);
+			});
+			return sum;
+		}
+	});
+
+	me.totalCarbonSecond = ko.pureComputed({
+		read: function() {
+			var sum = 0;
+			me.secondResults().forEach(function(r) {
+				sum += parseFloat(r.details.carbon);
+			});
+			return sum;
+		}
+	});
 } // end of ko model
 
 ko.applyBindings(new MyModel());
